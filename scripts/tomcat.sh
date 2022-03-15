2fb7448526bb86e42071cd86b1ba9eeee2ac49da
anand <96283755+Anand997758@users.noreply.github.com>
2022-03-15 14:39:16 +0530

    Update tomcat.sh

--- scripts/tomcat.sh
+++ scripts/tomcat.sh
@@ -5,7 +5,7 @@ sudo amazon-linux-extras install epel -y
 sudo amazon-linux-extras install java-openjdk11 -y
 # install the tomcat
 cd /opt/
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.58/bin/apache-tomcat-9.0.58-windows-x64.zip
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.18/bin/apache-tomcat-10.0.18-windows-x64.zip
 # extract 
 sudo unzip apache-tomcat-9.0.58-windows-x64.zip
 # delete the zip

0081d658feb0c252981b8e692abe1a632273373f
anand <96283755+Anand997758@users.noreply.github.com>
2022-04-22 14:01:53 +0530

    Update apache.sh

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -18,3 +18,12 @@ yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
 echo “Hello World from $(hostname -f)” > /var/www/html/index.html
+
+
+#!/bin/bash
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+vi index.html
+cp -r * /var/www/html/index.html/
